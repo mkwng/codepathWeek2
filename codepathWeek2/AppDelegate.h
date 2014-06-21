893a884bf93ea971b092f27e6b375aa315d6cea2
Michael Wang <hello@mkwng.com>
2014-06-21 10:55:56 -0700

    Initial Commit

---
+++ codepathWeek2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  codepathWeek2
+//
+//  Created by Michael Wang on 6/21/14.
+//  Copyright (c) 2014 mkwng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
